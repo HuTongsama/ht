c0691da3e48bd791e0b1c63da78c0263f1600206
HuTongsama <hutongsama@163.com>
2017-12-21 14:19:08 +0800

    Create stonewt.h

---
+++ ch11/11.9.6/stonewt.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<iostream>
+
+class Stonewt
+{
+public:
+	enum Mode {stones,int_pound,float_pound};
+	Stonewt(double lbs);
+	Stonewt(int stn, double lbs);
+	Stonewt();
+	~Stonewt();
+	void setMode(Mode m);
+	Stonewt operator+(const Stonewt & s)const;
+	Stonewt operator-(const Stonewt & s)const;
+	Stonewt operator*(double n)const;
+	bool operator>(const Stonewt & s)const;
+	bool operator<(const Stonewt & s)const;
+	bool operator>=(const Stonewt & s)const;
+	bool operator<=(const Stonewt & s)const;
+	bool operator==(const Stonewt & s)const;
+	bool operator!=(const Stonewt & s)const;
+	friend Stonewt operator*(double n, const Stonewt & s) { return s*n; }
+	friend std::ostream & operator<<(std::ostream &os, const Stonewt s);
+
+	
+private:
+	enum {Lbs_per_stn=14};//pounds per stone
+	int stone;			  //whole stones
+	double pds_left;      //fractional pounds
+	double pounds;		  //entire weight in pounds
+	Mode mode;
+};
