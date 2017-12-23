d0cb8bab73c56d6273d7b5e3ba9124921692ed21
HuTongsama <hutongsama@163.com>
2017-12-23 14:36:58 +0800

    Create complex0.cpp

---
+++ ch11/11.9.7/complex0.cpp
@@ -0,0 +1,59 @@
+#include "complex0.h"
+
+Complex::Complex():real(0),imaginary(0)
+{
+}
+
+Complex::Complex(double a, double b):real(a),imaginary(b)
+{
+}
+
+Complex::~Complex()
+{
+}
+
+Complex Complex::operator+(const Complex & b) const
+{
+	return Complex(real+b.real,imaginary+b.imaginary);
+}
+
+Complex Complex::operator-(const Complex & b) const
+{
+	return Complex(real-b.real,imaginary-b.imaginary);
+}
+
+Complex Complex::operator*(const Complex & b) const
+{
+	double A = real*b.real - imaginary*b.imaginary;
+	double B = real*b.imaginary + imaginary*b.real;
+	return Complex(A,B);
+}
+
+Complex operator*(double n, const Complex & b)
+{
+	return Complex(n*b.real,n*b.imaginary);
+}
+
+Complex operator~(const Complex & b)
+{
+	return Complex(b.real,-b.imaginary);
+}
+
+std::ostream & operator<<(std::ostream &os, const Complex & b)
+{
+	os << '(' << b.real << ',' << b.imaginary << ')';
+	return os;
+}
+
+std::istream & operator>>(std::istream &is, Complex & b)
+{
+	double A, B;
+	std::cout << "Enter real:";
+	if (is >> A)
+	{
+		std::cout << "Enter imaginary:";
+		is >> B;
+		b = Complex(A, B);
+	}
+	return is;
+}
