6c2b12a84e363a50cbfd52872cc51ce5149697e0
HuTongsama <hutongsama@163.com>
2018-02-01 12:05:35 +0800

    Create main.cpp

---
+++ ch14/14.7.1/main.cpp
@@ -0,0 +1,29 @@
+#include"winec.h"
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "Enter name of wine: ";
+	char lab[50];
+	cin.getline(lab, 50);
+	cout << "Enter number of years: ";
+	int yrs;
+	cin >> yrs;
+
+	Wine holding(lab, yrs);//store label,years,give arrays yrs elements
+	holding.GetBottles(); //solicit input for year,bottle count 
+	holding.Show();       //display object contents
+
+	const int YRS = 3;
+	int y[YRS] = { 1993,1995,1998 };
+	int b[YRS] = { 48,60,72 };
+	//create new object ,initialize using data in arrays y and b
+	Wine more("Gushing Grape Red", YRS, y, b);
+	more.Show();
+	cout << "Total bottles for " << more.Label()
+		<< ": " << more.sum() << endl;
+	cout << "Bye\n";
+	system("pause");
+}
